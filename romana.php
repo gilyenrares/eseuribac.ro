226ab0d42f3f20aa4e3eb022e0b7c52692448d80
gilyenrares <gilyenrares@users.noreply.github.com>
2019-11-01 07:28:39 +0000

    eseuri doc sys

--- romana.php
+++ romana.php
@@ -8,8 +8,51 @@
 	<link rel="stylesheet" type="text/css" href="styles/style.css">
 </head>
 <body>
+<!-- Navigation -->
 <?php include 'sections/navigation.sec.php'; ?>
-
+<!-- Jumbotron -->
+<img src="images/jumbotron.jpg" class="img-fluid" alt="Responsive image">
+<!-- Main body content -->
+<div class="container">
+	<div class="shadow-lg p-3 mb-5 bg-white rounded">
+	<section>
+		<article>
+			<h1 class="display-1">Display 1</h1>
+			<h1 class="display-2">Display 2</h1>
+			<h1 class="display-3">Display 3</h1>
+			<h1 class="display-4">Display 4</h1>
+			<p>Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.</p>
+			<p>Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.</p></article>
+			<article>
+			<blockquote class="blockquote">
+  			<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  			<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+			</blockquote>
+		</article>
+	</section>
+	<section>
+		<form action="text-reader.php" method="post">
+			<input type="hidden" name="location" value="romana">
+		  <div class="form-group">
+		    <label for="exampleFormControlSelect2">Example multiple select</label>
+		    <select multiple name="doc-name" class="form-control" id="exampleFormControlSelect2" required="required">
+		      <option>Essay 1</option>
+		      <option>Essay 2</option>
+		      <option>Essay 3</option>
+		      <option>Essay 4</option>
+		      <option>Essay 5</option>
+		    </select>
+		  </div>
+		  <button type="submit" name="get-document-submit" class="btn btn-outline-primary">Deschide document</button>
+		</form>
+	</section>
+	<section>
+		<article>
+			
+		</article>
+	</section>
+	</div>
+</div>
 
 
 
--- sections/navigation.sec.php
+++ sections/navigation.sec.php
@@ -33,27 +33,31 @@
             </ul>';
     break;
     case 'romana.php':
-      echo ' <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Acasa</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle active" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">Materia</a>
-    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-        <a class="dropdown-item  active" href="romana.php">Limba si Literarura Romana</a>
-        <a class="dropdown-item" href="istorie.php">Istorie</a>
-    </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="la-cafea.php">La Cafea</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="despre-noi.php">Despre noi</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="contact.php">Contact</a>
-      </li>
-    </ul>';
+    echo '<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Acasa</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle active" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">Materia</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item  active" href="romana.php">Limba si Literarura Romana</a>
+              <a class="dropdown-item" href="istorie.php">Istorie</a>
+            </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="la-cafea.php">La Cafea</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="despre-noi.php">Despre noi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>';
     break;
     case 'istorie.php':
     echo '<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
@@ -76,7 +80,11 @@
             <li class="nav-item">
               <a class="nav-link" href="contact.php">Contact</a>
             </li>
-          </ul>';
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>';
     break;
     case 'la-cafea.php':
     echo '<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
@@ -171,9 +179,5 @@
       </ul>';
 break;
 } ?>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>

42993af7a8816558236830be1eac021b8c194104
gilyenrares <gilyenrares@users.noreply.github.com>
2019-11-02 18:33:35 +0000

    index fz1

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="styles/style.css">
 </head>
 <body>
@@ -17,10 +18,7 @@
 	<div class="shadow-lg p-3 mb-5 bg-white rounded">
 	<section>
 		<article>
-			<h1 class="display-1">Display 1</h1>
-			<h1 class="display-2">Display 2</h1>
-			<h1 class="display-3">Display 3</h1>
-			<h1 class="display-4">Display 4</h1>
+			<h1 class="display-3 text-center">Bun venit pe Eseuri Bac</h1>
 			<p>Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.</p>
 			<p>Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.</p></article>
 			<article>
@@ -30,106 +28,64 @@
 			</blockquote>
 		</article>
 	</section>
+	<!-- Carousel top links -->
 	<section>
-		<article>
-			<!-- Card Deck -->
-			<div class="card-deck">
-			  <div class="card">
-			    <img src="images/jumbotron.jpg" class="card-img-top" alt="...">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			    </div>
-			    <div class="card-footer">
-			      <small class="text-muted">Last updated 3 mins ago</small>
-			    </div>
-			  </div>
-			  <div class="card">
-			    <img src="images/jumbotron.jpg" class="card-img-top" alt="...">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-			    </div>
-			    <div class="card-footer">
-			      <small class="text-muted">Last updated 3 mins ago</small>
-			    </div>
-			  </div>
-			  <div class="card">
-			    <img src="images/jumbotron.jpg" class="card-img-top" alt="...">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-			    </div>
-			    <div class="card-footer">
-			      <small class="text-muted">Last updated 3 mins ago</small>
-			    </div>
-			  </div>
-			</div>
-		</article>
+	<h2 class="display-4 text-center">Cele mai căutate informații</h2>
+	<div class="bd-example">
+	  <div id="carousel-index" class="carousel slide" data-ride="carousel">
+	    <ol class="carousel-indicators">
+	      <li data-target="#carousel-index" data-slide-to="0" class="active"></li>
+	      <li data-target="#carousel-index" data-slide-to="1"></li>
+	      <li data-target="#carousel-index" data-slide-to="2"></li>
+	    </ol>
+	    <div class="carousel-inner">
+	      <div class="carousel-item active">
+	        <img src="images/old-scroll.png" class="d-block w-100" alt="...">
+	        <div class="carousel-caption d-md-block">
+	          <h2>Programa la Istorie</h2>
+	          <a href="#">Mai multe...</button></a>
+	        </div>
+	      </div>
+	      <div class="carousel-item">
+	        <img src="images/old-scroll.png" class="d-block w-100" alt="...">
+	        <div class="carousel-caption d-md-block">
+	          <h2>Programa la Romană</h2>
+	          <a href="#">Mai multe...</button></a>
+	        </div>
+	      </div>
+	      <div class="carousel-item">
+	        <img src="images/old-scroll.png" class="d-block w-100" alt="...">
+	        <div class="carousel-caption d-md-block">
+	          <h2>Eseuri Romană</h2>
+	          <a href="#">Mai multe...</button></a>
+	        </div>
+	      </div>
+	      <div class="carousel-item">
+	        <img src="images/old-scroll.png" class="d-block w-100" alt="...">
+	        <div class="carousel-caption d-md-block">
+	          <h2>Eseuri Istorie</h2>
+	          <a href="#">Mai multe...</button></a>
+	        </div>
+	      </div>
+	    </div>
+	    <a class="carousel-control-prev" href="#carousel-index" role="button" data-slide="prev">
+	      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	      <span class="sr-only">Previous</span>
+	    </a>
+	    <a class="carousel-control-next" href="#carousel-index" role="button" data-slide="next">
+	      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	      <span class="sr-only">Next</span>
+	    </a>
+	  </div>
+	</div>
 	</section>
+	<!-- Videoclip de prezentare -->
+	<section></section>
 	</div>
 </div>
-<!-- Carousel -->
-<div class="bd-example">
-  <div id="carousel-index" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carousel-index" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-index" data-slide-to="1"></li>
-      <li data-target="#carousel-index" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="images/jumbotron.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="images/jumbotron.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="images/jumbotron.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carousel-index" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carousel-index" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-<!-- More Content -->
-<div class="container">
-	<div class="shadow-lg p-3 mb-5 bg-white rounded">
-	<section>
-		<article>
-			<h1>Display 1</h1>
-			<h2>Display 2</h2>
-			<h3>Display 3</h3>
-			<h4>Display 4</h4>
-			<p>Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.</p>
-			<p>Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.Text in here, text in there, basicaly text everywhere.</p></article>
-			<article>
-			<blockquote class="blockquote">
-  			<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-  			<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-			</blockquote>
-		</article>
-	</section>
-	</div>
-</div>
+
+
+
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
